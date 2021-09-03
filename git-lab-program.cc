065012439bdf2550a3eb7b01807f47c8b330a3d0
Keegan Goecke <kg333920@ohio.edu>
2021-09-03 15:28:38 -0400

    Program updates

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Keegan Goecke
+ *        Date: 09/03/2021
+ * Description: Program for Lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
